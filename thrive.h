0b519ad357a810e1590d16c952e6df7c628269fa
nickscha <nick@scharnbeck.de>
2026-04-03 10:25:53 +0200

    [OPTIMIZER] added more binary fold expressions

--- thrive.h
+++ thrive.h
@@ -1650,8 +1650,20 @@ THRIVE_API thrive_ast *thrive_ast_fold(thrive_ast *node)
             case THRIVE_TOKEN_KIND_EQUALS:
                 result = (a == b);
                 break;
+            case THRIVE_TOKEN_KIND_NOT_EQUALS:
+                result = (a != b);
+                break;
             case THRIVE_TOKEN_KIND_LT:
                 result = (a < b);
+                break;
+            case THRIVE_TOKEN_KIND_GT:
+                result = (a > b);
+                break;
+            case THRIVE_TOKEN_KIND_LT_EQUALS:
+                result = (a <= b);
+                break;
+            case THRIVE_TOKEN_KIND_GT_EQUALS:
+                result = (a >= b);
                 break;
             case THRIVE_TOKEN_KIND_AND_BITWISE:
                 result = a & b;
